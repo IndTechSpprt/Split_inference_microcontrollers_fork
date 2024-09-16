1bac6da299d3a35be9bda6be8065652333815fab
Shashwath <S.Suresh-3@student.tudelft.nl>
2024-09-16 15:00:05 +0200

    user inputs for role and ID

--- prep_setup.sh
+++ prep_setup.sh
@@ -3,21 +3,41 @@
 #Enable errors
 set -e
 
-#temp variable
-mcu_role_id="worker_0"
+echo "This script will configure your teensy for inference"
+
+read -p "Please specify the role and ID of the device to be configured in the following format - <role>_<ID>" mcu_role_id
+
 export mcu_role_id
 
-## Script to put the controller into download mode, upload weight fragments and then flash worker code
-echo "flashing teensy 4.1 to put it into download mode"
+read -p "Is the device connected to the PC? (Y/N)" ynconn
+case $ynconn in 
+    [yY]*);;
+    *) echo "Check connection and try again" 
+        exit;;
+esac
+
+echo "configuring teensy 4.1 as $mcu_role_id"
+
 cd ./MCU_code/PlatformIO_code/download
 pio run --target upload
-#TODO add success check before proceeding
-echo "Done, now attempting to download weights..."
-sleep 5
+echo "Waiting for COM connection"
+sleep 5 &
+wait
+echo "Attempting to download weights..."
 cd ../../
 python ./write_into_mcus.py /dev/ttyACM0
 echo "now flashing worker code and setting up communication..."
 cd ./PlatformIO_code/worker_code
 pio run --target upload
-#Check if success before saying done
+sleep 5 &
+wait
 echo "----DONE!----"
+#echo "checking if the configuration was successful"
+#TODO dynamic
+#ping -c 5 169.254.71.124
+#if [$? -eq 0]; then
+#    echo "----DONE!----"
+#else
+#    echo "FAILED!"
+#    exit
+#fi
