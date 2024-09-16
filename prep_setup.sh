c1af703054c59e9dda4771f3c189c49d2de3879e
Shashwath <S.Suresh-3@student.tudelft.nl>
2024-09-16 13:29:40 +0200

    functional scrip

--- prep_setup.sh
+++ prep_setup.sh
@@ -3,14 +3,21 @@
 #Enable errors
 set -e
 
+#temp variable
+temp="worker_0"
+export temp
+
 ## Script to put the controller into download mode, upload weight fragments and then flash worker code
 echo "flashing teensy 4.1 to put it into download mode"
 cd ./MCU_code/PlatformIO_code/download
-pio run --target upload
-echo "Done, now attempting to download weights"
+#pio run --target upload
+#TODO add success check before proceeding
+echo "Done, now attempting to download weights..."
 sleep 5
 cd ../../
-python ./write_into_mcus.py /dev/ttyACM0
+#python ./write_into_mcus.py /dev/ttyACM0
 echo "now flashing worker code and setting up communication..."
 cd ./PlatformIO_code/worker_code
+pio run --target upload
+#Check if success before saying done
 echo "----DONE!----"
