4f11cd1b13e5456929a07b295093febe47e87b54
Shashwath <S.Suresh-3@student.tudelft.nl>
2024-09-16 13:32:06 +0200

    flash steps added

--- prep_setup.sh
+++ prep_setup.sh
@@ -10,12 +10,12 @@ export temp
 ## Script to put the controller into download mode, upload weight fragments and then flash worker code
 echo "flashing teensy 4.1 to put it into download mode"
 cd ./MCU_code/PlatformIO_code/download
-#pio run --target upload
+pio run --target upload
 #TODO add success check before proceeding
 echo "Done, now attempting to download weights..."
 sleep 5
 cd ../../
-#python ./write_into_mcus.py /dev/ttyACM0
+python ./write_into_mcus.py /dev/ttyACM0
 echo "now flashing worker code and setting up communication..."
 cd ./PlatformIO_code/worker_code
 pio run --target upload
