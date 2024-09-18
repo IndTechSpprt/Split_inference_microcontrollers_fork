fcdacdba250868dcb10e8964a8fb4d0b9ef602b3
Shashwath Suresh <48093947+IndTechSpprt@users.noreply.github.com>
2024-09-18 15:03:19 +0200

    Removed incorrect cast

--- MCU_code/PlatformIO_code/worker_code/network_config.py
+++ MCU_code/PlatformIO_code/worker_code/network_config.py
@@ -8,6 +8,6 @@ id_int = int(id)
 
 # Get other parameters from ID
 ip_end = str(124 - id_int)
-mac_end = str(0xEB + hex(id))
+mac_end = str(0xEB + id)
 
 print("'-DMCU_ID=%s' '-DIP_END=%s' '-DMAC_END=%s'" % id % ip_end % mac_end)
