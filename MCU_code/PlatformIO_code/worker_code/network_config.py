7d516da065350fda35e2d91a8d41bb46e2a1ee94
Shashwath Suresh <48093947+IndTechSpprt@users.noreply.github.com>
2024-09-18 15:05:02 +0200

    Use int and not string for addition

--- MCU_code/PlatformIO_code/worker_code/network_config.py
+++ MCU_code/PlatformIO_code/worker_code/network_config.py
@@ -8,6 +8,6 @@ id_int = int(id)
 
 # Get other parameters from ID
 ip_end = str(124 - id_int)
-mac_end = str(0xEB + id)
+mac_end = str(0xEB + id_int)
 
 print("'-DMCU_ID=%s' '-DIP_END=%s' '-DMAC_END=%s'" % id % ip_end % mac_end)
